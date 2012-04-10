558acd311e14eb4dde100e705a3f72138bccd44c
huxl <huxl@huxl-Inspiron-N4050.(none)>
2012-04-10 13:33:21 +0800

    production default_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Lovelou::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'http://cold-snow-5967.heroku.com'}
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
