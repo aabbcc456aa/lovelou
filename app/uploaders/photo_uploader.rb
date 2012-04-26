6eaaed6861aafc55b7cc0044437175e83761c3d1
huxl <huxl@huxl-Inspiron-N4050.(none)>
2012-04-26 14:44:29 +0800

    aa

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,47 +1,51 @@
 # encoding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-#   include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-#  permissions 0777
-#  storage :file
- storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "public/uploaders/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
- def default_url
-   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
- end
-
-  # Process files as they are uploaded:
-#   process :scale => [600, 600]
+  include CarrierWave::MiniMagick
+  storage :fog
+   def store_dir
+     "devise_uploader"
+   end
+  #  # Include RMagick or MiniMagick support:
+  ##   include CarrierWave::RMagick
+  #   include CarrierWave::MiniMagick
   #
-   def scale(width, height)
-  #   # do something
-   end
-
-  # Create different versions of your uploaded files:
-   version :thumb do
-     process :resize_to_limit => [150, 150]
-   end
-   
-   version :big do
-     process :resize_to_limit => [450, 450]
-   end
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-   def extension_white_list
-     %w(jpg jpeg gif png)
-   end
+  #  # Choose what kind of storage to use for this uploader:
+  ##  permissions 0777
+  ##  storage :file
+  #
+  #
+  #  # Override the directory where uploaded files will be stored.
+  #  # This is a sensible default for uploaders that are meant to be mounted:
+  #  def store_dir
+  #    "public/uploaders/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #  end
+  #
+  #  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+  #
+  #  # Process files as they are uploaded:
+  ##   process :scale => [600, 600]
+  #  #
+  #   def scale(width, height)
+  #  #   # do something
+  #   end
+  #
+  #  # Create different versions of your uploaded files:
+  #   version :thumb do
+  #     process :resize_to_limit => [150, 150]
+  #   end
+  #   
+  #   version :big do
+  #     process :resize_to_limit => [450, 450]
+  #   end
+  #  # Add a white list of extensions which are allowed to be uploaded.
+  #  # For images you might use something like this:
+  #   def extension_white_list
+  #     %w(jpg jpeg gif png)
+  #   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
