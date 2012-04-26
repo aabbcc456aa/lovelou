a8c27c66b67fd140c2de73491cbde9b30a3b6933
huxl <huxl@huxl-Inspiron-N4050.(none)>
2012-04-26 16:50:58 +0800

    aa

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,7 +6,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
     
   # Include RMagick or MiniMagick support:
 #   include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
 #  permissions 0777
